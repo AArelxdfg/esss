cf8af371316668b30098924fbd8fa34a9d1dfae3
AArelxdfg <167829246+AArelxdfg@users.noreply.github.com>
2026-08-24 00:19:31 +0300

    AArelOS: add LLera native service entry point

---
+++ aarelos-ci/LLeraService/main.cpp
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+#include "ConnectionFromClient.h"
+#include <LibCore/EventLoop.h>
+#include <LibCore/System.h>
+#include <LibIPC/MultiServer.h>
+#include <LibMain/Main.h>
+
+ErrorOr<int> serenity_main(Main::Arguments)
+{
+    TRY(Core::System::pledge("stdio recvfd sendfd accept unix"));
+    Core::EventLoop event_loop;
+    auto server = TRY(IPC::MultiServer<LLeraService::ConnectionFromClient>::try_create());
+    TRY(Core::System::unveil(nullptr, nullptr));
+    return event_loop.exec();
+}

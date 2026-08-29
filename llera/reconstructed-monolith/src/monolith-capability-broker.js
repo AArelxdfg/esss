467bc4e5ab7f98b425baee1f62e1902cc727b97d
AArelxdfg <167829246+AArelxdfg@users.noreply.github.com>
2026-08-29 22:46:07 +0300

    llera: expand executable historical capability surface

--- llera/reconstructed-monolith/src/monolith-capability-broker.js
+++ llera/reconstructed-monolith/src/monolith-capability-broker.js
@@ -1,6 +1,29 @@
 'use strict';
 
 const CAPABILITY_TOOL_BINDINGS = Object.freeze({
+  // Historical V2 filesystem/process surface restored as executable service bindings.
+  list_dir: ['filesystem', 'listDir'],
+  read_file: ['filesystem', 'readFile'],
+  write_file: ['filesystem', 'writeFile'],
+  apply_patch: ['filesystem', 'applyPatch'],
+  search_files: ['filesystem', 'searchFiles'],
+  make_dir: ['filesystem', 'makeDir'],
+  copy_path: ['filesystem', 'copyPath'],
+  move_path: ['filesystem', 'movePath'],
+  delete_path: ['filesystem', 'deletePath'],
+  read_text_range: ['filesystem', 'readTextRange'],
+  file_stat: ['filesystem', 'stat'],
+  path_exists: ['filesystem', 'exists'],
+  hash_file: ['filesystem', 'hashFile'],
+
+  run_command: ['processes', 'runCommand'],
+  start_process: ['processes', 'start'],
+  process_status: ['processes', 'status'],
+  process_stop: ['processes', 'stop'],
+  list_processes: ['processes', 'list'],
+  read_process_output: ['processes', 'readOutput'],
+
+  // Reconstructed MONOLITH capability services.
   vision_analyze_image: ['vision', 'analyze'],
   vision_ocr_screen: ['vision', 'ocrScreen'],
   evidence_record: ['evidence', 'record'],
@@ -17,8 +40,16 @@ const CAPABILITY_TOOL_BINDINGS = Object.freeze({
   llera_bench: ['diagnostics', 'bench']
 });
 
+const DIRECT_ARG_TOOLS = new Set([
+  'list_dir','read_file','write_file','apply_patch','search_files','make_dir','copy_path','move_path','delete_path',
+  'read_text_range','file_stat','path_exists','hash_file',
+  'run_command','start_process','process_status','process_stop','list_processes','read_process_output'
+]);
+
 class MonolithCapabilityBroker {
   constructor({
+    filesystem,
+    processes,
     vision,
     evidence,
     updater,
@@ -31,6 +62,7 @@ class MonolithCapabilityBroker {
     diagnostics
   } = {}) {
     Object.assign(this, {
+      filesystem, processes,
       vision, evidence, updater, hostguard, outcomeMemory,
       autonomy, knowledgeGraph, skills, snapshots, diagnostics
     });
@@ -59,6 +91,11 @@ class MonolithCapabilityBroker {
   }
 
   async invoke(tool, args = {}, context = {}) {
+    if (DIRECT_ARG_TOOLS.has(tool)) {
+      const [serviceName, methodName] = CAPABILITY_TOOL_BINDINGS[tool];
+      return this._call(serviceName, methodName, `${serviceName} capability unavailable`, { ...args, context });
+    }
+
     switch (tool) {
       case 'vision_analyze_image':
         return this._call('vision', 'analyze', 'vision pipeline unavailable',
@@ -144,4 +181,4 @@ function compact(value) {
   return Object.fromEntries(Object.entries(value || {}).filter(([, v]) => v !== undefined));
 }
 
-module.exports = { MonolithCapabilityBroker, CAPABILITY_TOOL_BINDINGS };
+module.exports = { MonolithCapabilityBroker, CAPABILITY_TOOL_BINDINGS, DIRECT_ARG_TOOLS };
